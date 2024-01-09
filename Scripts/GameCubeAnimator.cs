ecf6deb126e7c37b174cbdc005189cdd5019c41f
Sofilinya <152196593+Sofilinya@users.noreply.github.com>
2024-01-09 20:28:19 +0300

    Add files via upload

--- Scripts/GameCubeAnimator.cs
+++ Scripts/GameCubeAnimator.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 public class GameCubeAnimator : MonoBehaviour
 {
-    public Animation CubeAnimation;         // Ссылка на компонент анимации кубика
-    public GameCubeThrower GameCubeThrower; // Скрипт бросков кубика
+    private GameCubeThrower GameCubeThrower;        // Скрипт бросков кубика
+    private Animation CubeAnimation = null;         // Ссылка на компонент анимации кубика
 
+    void Start()
+    {
+        CubeAnimation = GetComponent<Animation>();
+    }
     // Проигрываем анимацию
-    public void PlayAnimation() { CubeAnimation.Play(); }
+    public void PlayAnimation(GameCubeThrower gameCubeThrower)
+    {
+        GameCubeThrower = gameCubeThrower;
+        CubeAnimation.Play();
+    }
 
     // Этот метод мы вызовем позже внутри анимации
     public void OnAnimationEnd() { GameCubeThrower.ContinueAfterCubeAnimation(); }  // Продолжаем действия после анимации
+
+    public AnimationClip GetAnimationClip() { return CubeAnimation.clip; }
+    public void SetAnimationClip(AnimationClip animationClip) { CubeAnimation.clip = animationClip; }
+    public Transform GetTransform() { return transform; }
 }
